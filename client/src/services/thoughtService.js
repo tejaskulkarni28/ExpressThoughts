4bf7a462a70083b20d7745e6b320c80e02117f08
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-10-11 18:06:47 +0530

    react->node->mongo{data: insertion is being done}

---
+++ client/src/services/thoughtService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const thoughtService = (props)=>{
+    axios.post('http://localhost:3001/thought/add', {data:props})
+    .then((response)=>{
+        console.log(response)
+    })
+    .catch((err)=>{
+        if(err){
+            console.log("Error: " + err)
+        }
+    })
+}
+export default thoughtService
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors())
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/thoughtsDB').then(()=>{
     console.log("MongoDB connected successfully!")
@@ -8,8 +11,31 @@ mongoose.connect('mongodb://localhost:27017/thoughtsDB').then(()=>{
     console.log(err)
 })
 
+// Middleware to parse incoming JSON requests
+app.use(express.json());
+
 app.get('/', (_, res)=>{
     res.send("RESTFUL API:  REPRESNTATIONAL STATE TRANSFER APPLICATION INTERFACE")
+})
+
+
+const thoughtmodel =  require('./models/thoughts');
+app.post('/thought/add', async(req, res)=>{
+    console.log(req.body)
+
+    // Insert the data into mongodb
+    const thought = req.query.thought;
+    let temp_thought = await thoughtmodel.findOne({thought})
+    if(req.body.data != ''){
+        if(!temp_thought){
+            temp_thought = new thoughtmodel({
+                thought: req.body.data
+            })
+        }
+    }else{
+        console.log('Data not invoked!')
+    }
+    await temp_thought.save();
 })
 
 const port = 3001; // localhost port 
