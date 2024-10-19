7d3d2d487ba2389a72f046e640f4b74695453bc8
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-10-19 09:04:03 +0530

    Articulated the code a bit

--- server/controllers/addController.js
+++ server/controllers/addController.js
@@ -1,19 +1,18 @@
 const thoughtmodel = require('../models/thoughts')
 
 const addThought = async (req, res)=>{
-        const thought = req.query.thought;
-        let temp_thought = await thoughtmodel.findOne({thought})
-        
-        if(req.body.data != ''){
-                if(!temp_thought){
-                        temp_thought = new thoughtmodel({
+        let thought = req.body.data;
+        if(Boolean(thought)){
+                await new thoughtmodel({
                                 thought: req.body.data
-                        })
-                }
+                }).save().then((value)=>{
+                        console.log('Promised resolved with: ', value)
+                }).catch((error)=>{
+                        console.log(error)
+                })
         }else{
-                console.log('data not invoked!')
+                console.log("error saving the data")
         }
-        await temp_thought.save();
 }
 
 module.exports = {addThought}
