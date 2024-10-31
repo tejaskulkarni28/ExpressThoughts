3c37723867480b2abe0c0d916b247b77875a3a4c
Tejas Kulkarni <tejaskulkarni2801@gmail.com>
2024-10-31 22:43:01 +0530

    Added user model and updated new fields in thoughts model

--- server/models/thoughts.js
+++ server/models/thoughts.js
@@ -3,7 +3,11 @@ const schema = mongoose.Schema;
 
 
 const thoughtSchema = new schema({
-    thought:{type: String}
+    userId: {type: schema.Types.ObjectId, ref: 'User', required: true},
+    thought: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now},
+    likes: [{type: schema.Types.ObjectId, ref: 'User'}],
+    tags: [{type: String}]
 })
 
 module.exports = mongoose.model('thoughts', thoughtSchema) // Mongoose automatically pluralizes the name you provide. So, in this case:
